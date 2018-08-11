e9707e84843a57b815d906d4707556287711fa7b
futia <futianyi1994@126.com>
2018-08-11 11:54:06 +0800

    Initial commit

---
+++ App.js
@@ -0,0 +1,18 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+
+var LoginView = require('./loginView');
+export default class Login extends Component {
+    render() {
+        return (
+            <LoginView/>
+        );
+    }
+}
